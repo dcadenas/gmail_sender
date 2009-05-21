da6b54ba10cac9c95418b56ff1381e1ac26f5f70
Daniel Cadenas <dcadenas@gmail.com>
2009-05-21 05:00:52 -0300

    Initial commit to gmail_sender.

---
+++ lib/gmail_sender.rb
@@ -0,0 +1,29 @@
+require "net/smtp"
+
+class GmailSender
+  def initialize(gmail_user, gmail_password)
+    @gmail_user = "#{gmail_user}@gmail.com"
+    @gmail_password = gmail_password
+  end
+
+  def create_message(to, subject, content)
+<<MSG
+From: #@gmail_user
+To: #{to}
+Subject: #{subject}
+
+#{content}
+MSG
+  end
+
+  def send(to, subject, content)
+    net_smtp = Net::SMTP.new("smtp.gmail.com", 587)
+    net_smtp.enable_starttls 
+    net_smtp.start("gmail.com", @gmail_user, @gmail_password, :plain) do |smtp|
+      msg = create_message(to, subject, content)
+      puts msg
+      smtp.send_message(msg, @gmail_user, to)
+    end
+  end
+end
+
