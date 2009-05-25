c53bfa6aa64207112c3489c37002da113019daf6
Daniel Cadenas <dcadenas@gmail.com>
2009-05-25 01:20:32 -0300

    Added expectations test (for you oboxodo!!)

--- test/gmail_sender_test.rb
+++ test/gmail_sender_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
-class GmailSenderTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+Expectations do
+  expect "From: gmail_account_name@gmail.com\nTo: to@gmail.com\nSubject: subject\n\nbody\n" do
+    gmail_sender = GmailSender.new("gmail_account_name", "gmail_account_password")
+    gmail_sender.create_message("to@gmail.com", "subject", "body")
   end
 end
