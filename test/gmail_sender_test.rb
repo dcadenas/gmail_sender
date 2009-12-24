87e840610cc0c3b77dd3e03a01ec3814ec75d0a9
Daniel Cadenas <dcadenas@gmail.com>
2009-12-24 15:27:03 -0200

    Updated test

--- test/gmail_sender_test.rb
+++ test/gmail_sender_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 Expectations do
   expect "From: gmail_account_name@gmail.com\nTo: to@gmail.com\nSubject: subject\n\nbody\n" do
     gmail_sender = GmailSender.new("gmail_account_name", "gmail_account_password")
-    gmail_sender.create_message("to@gmail.com", "subject", "body")
+    string_io = StringIO.new
+    gmail_sender.set_message_stream(string_io, "to@gmail.com", "subject", "body")
+    string_io.string
   end
 end
