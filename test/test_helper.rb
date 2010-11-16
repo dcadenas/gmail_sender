8acf8ba363d315b72e8c281bdd26e1b9c6825db5
Daniel Cadenas <dcadenas@gmail.com>
2010-11-16 17:43:12 -0200

    Merged elcuervo's filetesthelper fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'expectations'
+begin
 require 'file_test_helper'
+rescue LoadError
+  puts "Please install filetesthelper to run the tests: gem install filetesthelper"
+  exit 1
+end
+
 include FileTestHelper
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
